d38984d09156ba1f37a316cf3e165cb8db45fd54
Michael Mulich <michael.mulich@gmail.com>
2017-11-22 12:07:30 -0800

    Add test for lxml ojbect argument
    This completes the coverage for this function.

--- nebuchadnezzar/tests/test_converters.py
+++ nebuchadnezzar/tests/test_converters.py
@@ -6,13 +6,17 @@
 # See LICENCE.txt for details.
 # ###
 import os
-import re
+import sys
+from io import BytesIO
 try:
     from importlib import reload
 except ImportError:
     pass  # reload() is a built-in global in py2
 
+from lxml import etree
+
 from cnxdb.triggers.transforms.converters import (
+    DEFAULT_XMLPARSER,
     cnxml_to_full_html,
     html_to_full_cnxml,
 )
@@ -29,16 +33,6 @@ class BaseTestCase(object):
 
 class TestCnxml2Html(BaseTestCase):
 
-    def test_dev_ctoh_version(self):
-        import rhaptos.cnxmlutils
-        # Mimic the in-database environment: no path
-        os.environ['PATH'] = ''
-        reload(rhaptos.cnxmlutils)
-        cnxml = self.get_file('m42033-1.3.cnxml')
-        content = cnxml_to_full_html(cnxml)
-
-        assert '0+unknown' not in content
-
     def test_success(self):
         # Case to test the transformation of cnxml to html.
         cnxml = self.get_file('m42033-1.3.cnxml')
@@ -52,12 +46,35 @@ class TestCnxml2Html(BaseTestCase):
         import rhaptos.cnxmlutils
         assert rhaptos.cnxmlutils.__version__ in content
 
+    def test_dev_cxnml2html_version(self):
+        import rhaptos.cnxmlutils
+        # Mimic the in-database environment: no path
+        os.environ['PATH'] = ''
+        reload(rhaptos.cnxmlutils)
+        cnxml = self.get_file('m42033-1.3.cnxml')
+        content = cnxml_to_full_html(cnxml)
+
+        assert '0+unknown' not in content
+
 
 class TestHtml2Cnxml(BaseTestCase):
 
     def test_success(self):
         # Case to test the transformation of cnxml to html.
         html = self.get_file('m42033-1.3.html')
+
+        content = html_to_full_cnxml(html)
+
+        # Check for partial conversion.
+        # rhaptos.cnxmlutils has tests to ensure full conversion.
+        assert b'<document' in content
+
+    def test_with_passed_in_etree(self):
+        # Case to test the transformation of cnxml to html.
+        html = self.get_file('m42033-1.3.html')
+        if sys.version_info > (3,) and isinstance(html, str):
+            html = html.encode('utf-8')
+        html = etree.parse(BytesIO(html), DEFAULT_XMLPARSER)
 
         content = html_to_full_cnxml(html)
 
