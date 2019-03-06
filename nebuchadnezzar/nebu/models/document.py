a2642f305ca3a658c0e404f2db98487c8343146c
Michael Mulich <michael.mulich@gmail.com>
2019-03-05 21:30:30 -0800

    Add functionality to natively support the Document type
    This adds functionality to support the cnx-epub Document type natively to this application. This implementation is more intelligent than a standard Document, specifically about handling its resources and references to those resources.
    
    Some of this change should make its way upstream to cnx-epub.

---
+++ nebuchadnezzar/nebu/models/document.py
@@ -0,0 +1,184 @@
+import io
+from pathlib import Path
+
+from lxml import etree
+from cnxepub.html_parsers import (
+    HTML_DOCUMENT_NAMESPACES,
+    parse_metadata,
+)
+from cnxepub.models import (
+    Document as BaseDocument,
+    Resource,
+)
+from cnxml.parse import parse_metadata as parse_cnxml_metadata
+from cnxtransforms import cnxml_to_full_html
+
+from .utils import convert_to_model_compat_metadata
+
+# A list of filenames to ignore while attempting to discover
+# resources in a directory.
+IGNORE_RESOURCES_BY_FILENAME = (
+    '.sha1sum',
+    'index.cnxml',
+)
+
+
+def _id_from_metadata(metadata):
+    """Given an item's metadata, discover the id."""
+    identifier = "cnx-archive-uri"
+    return metadata.get(identifier)
+
+
+class Document(BaseDocument):
+
+    def __init__(self, *args, **kwargs):
+        self._reference_resolver = kwargs.get('reference_resolver')
+        super(Document, self).__init__(*args, **kwargs)
+
+        # Resolve the references before returning the object
+        self.resolve_references()
+
+    @classmethod
+    def from_index_cnxml(cls, filepath, reference_resolver):
+        """\
+        Given a ``index.cnxml`` as ``filepath``. This provides the same
+        interface as :class:`cnxepub.models.Document`, but can be created
+        from file.
+
+        Note, the ``reference_resolver`` is a function that accepts the
+        reference and resource as it's arguments. It's the resolver's
+        responsibility to *bind* the reference to the resource.
+
+        :param filepath: location of the ``index.cnxml``
+        :type filepath: :class:`pathlib.Path`
+        :param reference_resolver: function used to resolve references
+        :type reference_resolver: func
+        :return: Document object
+        :rtype: :class:`Document`
+
+        """
+        # Parse to xml for metadata extraction
+        with filepath.open('rb') as fb:
+            cnxml = etree.parse(fb)
+        # Transform and parse to html for model creation
+        with filepath.open('r') as fb:
+            html = etree.fromstring(cnxml_to_full_html(fb.read()).encode())
+
+        resources = []
+        metadata = parse_cnxml_metadata(cnxml)
+        metadata = convert_to_model_compat_metadata(metadata)
+        id = _id_from_metadata(metadata)
+
+        # Clean and sanatize the content
+        content = cls._sanatize_content(html)
+
+        # Process the resource file
+        resources = cls._find_resources(filepath.parent)
+
+        # Create the object
+        return cls(id, content, metadata=metadata, resources=resources,
+                   reference_resolver=reference_resolver)
+
+    @classmethod
+    def from_filepath(cls, filepath):
+        """\
+        Given a html document as ``filepath``. This provides the same
+        interface as :class:`cnxepub.models.Document`, but can be created
+        from file.
+
+        :param filepath: location of the ``index.cnxml``
+        :type filepath: :class:`pathlib.Path`
+        :return: Document object
+        :rtype: :class:`Document`
+
+        """
+        with filepath.open('rb') as fb:
+            html = etree.parse(fb)
+
+        resources = []
+        metadata = parse_metadata(html)
+        id = _id_from_metadata(metadata)
+
+        # Clean and sanatize the content
+        content = cls._sanatize_content(html)
+
+        # This method will not process resources because it can not assume
+        # to know where the resources are. If loading of resources is needed,
+        # please load the resources from the references after instantiation.
+
+        # Create the object
+        return cls(id, content, metadata=metadata, resources=resources)
+
+    @staticmethod
+    def _sanatize_content(html):
+        """\
+        Sanatize the given HTML (as ``html``) of metadata.
+
+        :param html: element tree representing the document
+        :type html: etree.ElementTree
+        :return: html as a string
+        :rtype: str
+
+        """
+        body = html.xpath(
+            '//xhtml:body',
+            namespaces=HTML_DOCUMENT_NAMESPACES,
+        )[0]
+        metadata_nodes = html.xpath(
+            "//xhtml:body/*[@data-type='metadata']",
+            namespaces=HTML_DOCUMENT_NAMESPACES,
+        )
+        for node in metadata_nodes:
+            body.remove(node)
+        for key in body.keys():
+            if key in ('itemtype', 'itemscope'):
+                body.attrib.pop(key)
+        return etree.tostring(html)
+
+    @staticmethod
+    def _find_resources(loc):
+        """Given a location to look for resources, create and return a list of
+        :class:`cnxepub.models.Resource` objects.
+
+        :param loc: location to look for resource files
+        :type loc: :class:`pathlib.Path`
+        :return: list of Resources
+        :rtype: [:class:`cnxepub.models.Resource`]
+
+        """
+        resources = []
+        blank_data = io.BytesIO()
+        for filepath in loc.glob('*'):
+            if filepath.name in IGNORE_RESOURCES_BY_FILENAME:
+                continue
+            resources.append(
+                Resource(
+                    filepath.name, blank_data,
+                    media_type='n/a', filename=filepath.name,
+                )
+            )
+        return resources
+
+    def resolve_references(self):
+        """\
+        Resolve the object's internal references if we have a resource that
+        matches the reference's filename.
+
+        """
+        if self._reference_resolver is None:
+            # Without a resolver this method can't do anything.
+            return
+
+        for ref in self.references:
+            if ref.remote_type == 'external':
+                continue
+            name = Path(ref.uri).name
+            try:
+                resource = [r for r in self.resources if r.filename == name][0]
+            except IndexError:
+                # When resources are missing, the problem is pushed off
+                # to the rendering process, which will
+                # raise a missing reference exception when necessary.
+                pass
+            else:
+                self._reference_resolver(ref, resource)
---
+++ nebuchadnezzar/nebu/tests/models/test_document.py
@@ -0,0 +1,182 @@
+from copy import copy
+
+import pytest
+from lxml import etree
+from cnxepub.html_parsers import HTML_DOCUMENT_NAMESPACES
+
+from nebu.models.document import Document
+
+
+REFERENCE_MARKER = '#!--testing--'
+M46882_METADATA = {
+    'authors': [{'id': 'OpenStaxCollege',
+                 'name': 'OpenStaxCollege',
+                 'type': 'cnx-id'}],
+    'cnx-archive-shortid': None,
+    'cnx-archive-uri': 'm46882@1.17',
+    'copyright_holders': [{'id': 'OSCRiceUniversity',
+                           'name': 'OSCRiceUniversity',
+                           'type': 'cnx-id'}],
+    'created': '2013/07/19 00:42:23 -0500',
+    'derived_from_title': None,
+    'derived_from_uri': None,
+    'editors': [],
+    'illustrators': [],
+    'keywords': ('cumulative relative frequency', 'frequency'),
+    'language': 'en',
+    'license_text': 'CC BY',
+    'license_url': 'http://creativecommons.org/licenses/by/4.0/',
+    'print_style': None,
+    'publishers': [{'id': 'OpenStaxCollege',
+                    'name': 'OpenStaxCollege',
+                    'type': 'cnx-id'},
+                   {'id': 'cnxstats', 'name': 'cnxstats', 'type': 'cnx-id'}],
+    'revised': '2019/02/08 09:37:55.846 US/Central',
+    'subjects': ('Mathematics and Statistics',),
+    'summary': None,
+    'title': 'Frequency, Frequency Tables, and Levels of Measurement',
+    'translators': [],
+    'version': '1.17',
+}
+
+
+@pytest.fixture
+def collection_data(datadir):
+    """This data is the result of a ``neb get ...``"""
+    return datadir / 'collection_for_bakedpdf_workflow'
+
+
+@pytest.fixture
+def assembled_data(datadir):
+    """This data is the results of a ``neb assemble ...``"""
+    return datadir / 'assembled_collection_for_bakedpdf_workflow'
+
+
+def mock_reference_resolver(reference, resource):
+    """Used for testing reference resolution during model tests"""
+    reference.bind(resource, '{}/{{}}'.format(REFERENCE_MARKER))
+
+
+class TestDocument(object):
+
+    def test_sanatize_content(self, assembled_data):
+        with (assembled_data / 'm46913.xhtml').open('rb') as fb:
+            html = etree.parse(fb)
+            # And parse a second copy for verification
+            fb.seek(0)
+            expected_html = etree.parse(fb)
+
+        # Hit the target
+        results = Document._sanatize_content(html)
+
+        # Construct expected results
+        body = expected_html.xpath(
+            "//xhtml:body",
+            namespaces=HTML_DOCUMENT_NAMESPACES,
+        )[0]
+        metadata_elm = body.xpath(
+            "//xhtml:div[@data-type='metadata']",
+            namespaces=HTML_DOCUMENT_NAMESPACES,
+        )[0]
+        body.remove(metadata_elm)
+        body.attrib.pop('itemtype')
+        body.attrib.pop('itemscope')
+        expected_results = etree.tostring(expected_html)
+
+        assert results == expected_results
+
+    def test_find_resources(self, collection_data):
+        loc = collection_data / 'm46909'
+
+        # Hit the target
+        resources = Document._find_resources(loc)
+
+        # Verify we discovered the resource files
+        expected_filenames = [
+            'Prev_m16020_DotPlot.png',
+            'fig-ch01_02_01n.png',
+            'm16020_DotPlot_description.html',
+            'm16020_DotPlot_download.pdf',
+        ]
+        assert sorted([r.id for r in resources]) == expected_filenames
+        assert sorted([r.filename for r in resources]) == expected_filenames
+
+        # Ensure the resources are empty, because we don't need the data for
+        # our operation.
+        for r in resources:
+            with r.open() as open_r:
+                contents = open_r.read()
+            assert contents == b''
+            assert r.media_type == 'n/a'
+
+    def test_from_index_cnxml(self, collection_data):
+        filepath = collection_data / 'm46882' / 'index.cnxml'
+
+        # Hit the target
+        doc = Document.from_index_cnxml(filepath, mock_reference_resolver)
+
+        # Verify the metadata
+        assert doc.id == 'm46882'
+        expected_metadata = M46882_METADATA
+        assert doc.metadata == expected_metadata
+
+        # Verify the content is content'ish
+        assert doc._xml.xpath(
+            "/xhtml:body/*[@data-type='metadata']",
+            namespaces=HTML_DOCUMENT_NAMESPACES,
+        ) == []
+        assert len(doc._xml.xpath(
+            "//*[@id='fs-idm20141232']",
+            namespaces=HTML_DOCUMENT_NAMESPACES,
+        )) == 1
+
+        # Verify the resources are attached to the object
+        expected_filenames = [
+            'CNX_Stats_C01_M10_001.jpg',
+            'CNX_Stats_C01_M10_002.jpg',
+            'CNX_Stats_C01_M10_003.jpg',
+        ]
+        filenames = [r.filename for r in doc.resources]
+        assert sorted(filenames) == expected_filenames
+
+        # Verify the references have been rewritten
+        ref = '{}/CNX_Stats_C01_M10_003.jpg'.format(REFERENCE_MARKER).encode()
+        assert ref in doc.content
+        # Verify external and non-existent resource references remain
+        assert b'src="foobar.png"' in doc.content
+        assert b'ef="/m10275@2.1"' in doc.content  # rewritten in cnxml->html
+        assert b'ef="http://en.wikibooks.org/"' in doc.content
+
+    def test_from_filepath(self, assembled_data):
+        filepath = assembled_data / 'm46882.xhtml'
+
+        # Hit the target
+        doc = Document.from_filepath(filepath)
+
+        # Verify the metadata
+        assert doc.id == 'm46882'
+        expected_metadata = copy(M46882_METADATA)
+        # cnx-epub metadata is mutable, so sequences are lists rather than
+        # tuples.
+        expected_metadata['keywords'] = list(expected_metadata['keywords'])
+        expected_metadata['subjects'] = list(expected_metadata['subjects'])
+        assert doc.metadata == expected_metadata
+
+        # Verify the content is content'ish
+        assert doc._xml.xpath(
+            "/xhtml:body/*[@data-type='metadata']",
+            namespaces=HTML_DOCUMENT_NAMESPACES,
+        ) == []
+        assert len(doc._xml.xpath(
+            "//*[@id='fs-idm20141232']",
+            namespaces=HTML_DOCUMENT_NAMESPACES,
+        )) == 1
+
+        # Verify the resources are attached to the object
+        expected_filenames = []
+        filenames = [r.filename for r in doc.resources]
+        assert sorted(filenames) == expected_filenames
+
+        # Verify the references have been rewritten
+        ref = '{}/CNX_Stats_C01_M10_003.jpg'.format(REFERENCE_MARKER).encode()
+        assert ref in doc.content
