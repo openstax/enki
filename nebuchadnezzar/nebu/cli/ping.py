892c31cacac485574dc5dffdb30d54f1509eb009
Ripal Nathuji <rnathuji@neoteklabs.com>
2020-05-12 11:25:56 -0500

    Add insecure flag to neb get, ping, and publish
    For development and testing, we sometimes want to point neb at
    environments / endpoints that have self-signed certificates and fail
    SSL checks that are performed by default. For example, developers may
    want to run neb against a local VM running the cnx stack. This change
    adds a `-k`/`--insecure` flag to the get, ping, and publish commands
    so they can be invoked in a manner that ignores SSL check failures.
    
    The choice of `-k`/`--insecure` is based upon the curl command.

--- nebuchadnezzar/nebu/cli/ping.py
+++ nebuchadnezzar/nebu/cli/ping.py
@@ -12,14 +12,20 @@ from ._common import common_params, get_base_url, logger
 @click.argument('env')
 @click.option('-u', '--username', type=str, prompt=True)
 @click.option('-p', '--password', type=str, prompt=True, hide_input=True)
+@click.option('-k', '--insecure', is_flag=True, default=False,
+              help="Ignore SSL certificate verification errors")
 @click.pass_context
-def ping(ctx, env, username, password):
+def ping(ctx, env, username, password, insecure):
     """Check credentials and permission to publish on server"""
     base_url = get_base_url(ctx, env)
 
     auth = HTTPBasicAuth(username, password)
     auth_ping_url = '{}/api/auth-ping'.format(base_url)
-    auth_ping_resp = requests.get(auth_ping_url, auth=auth)
+    auth_ping_resp = requests.get(
+        auth_ping_url,
+        auth=auth,
+        verify=not insecure
+    )
 
     if auth_ping_resp.status_code == 401:
         logger.error('Bad credentials: \n{}'.format(
@@ -32,7 +38,11 @@ def ping(ctx, env, username, password):
         sys.exit(1)
 
     publish_ping_url = '{}/api/publish-ping'.format(base_url)
-    publish_ping_resp = requests.get(publish_ping_url, auth=auth)
+    publish_ping_resp = requests.get(
+        publish_ping_url,
+        auth=auth,
+        verify=not insecure
+    )
 
     if publish_ping_resp.status_code == 401:
         logger.error('Publishing not allowed: \n{}'.format(
