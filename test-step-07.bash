3a90a1fe723e6d9b3143fc964557c36924318815
philschatz <253202+philschatz@users.noreply.github.com>
2021-07-09 10:52:38 -0500

    speed up by converting only 1 docx file

--- test-step-07.bash
+++ test-step-07.bash
@@ -3,6 +3,17 @@ set -e
 
 SOCI_DIR=./data/test-soci
 
+xhtml_files_dir="$SOCI_DIR/gdocified/content"
+files=($xhtml_files_dir/*.xhtml)
+keep_file="${files[0]}"
+
+mv $keep_file $keep_file.keep
+
+echo "Note: Removing all xhtml files except one to speed up this test step: $keep_file"
+# find "$xhtml_files_dir" ! -name "$keep_file" -name '*.xhtml' -type f -exec rm -f {} +
+rm -rf $xhtml_files_dir/*.xhtml
+mv $keep_file.keep $keep_file
+
 # kcov causes this step to hang so skip the KCOV_DIR (probably the pm2 mathml2svg background process)
 SKIP_DOCKER_BUILD=1 \
 START_AT_STEP=archive-convert-docx \
