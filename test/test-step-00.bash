b03e9a71b47107edf5880462d5d5542443d0136f
philschatz <253202+philschatz@users.noreply.github.com>
2021-07-16 15:51:49 -0500

    include typescript code coverage

--- test/test-step-00.bash
+++ test/test-step-00.bash
@@ -3,12 +3,15 @@ set -e
 [[ ${TRACE_ON} ]] && set -x
 [[ $0 != "-bash" ]] && cd "$(dirname "$0")"
 
-cd ../build-concourse/
-npm install
+# Use --prefix to keep the .nyc_output at the root for codecov
+npm --prefix ../build-concourse/ install
 
 # Build concourse pipelines with dummy credentials just to make sure it runs
 CODE_VERSION=dummycodeversion \
 AWS_ACCESS_KEY_ID=dummyawskey \
 AWS_SECRET_ACCESS_KEY=dummyawssecret \
 GOOGLE_SERVICE_ACCOUNT_CREDENTIALS=dummygoogle \
-npm run coverage
+npm --prefix ../build-concourse/ run coverage
+
+# Move so codecov finds it
+mv ../build-concourse/.nyc_output ../
