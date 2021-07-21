3d92a6f4cf0f69bb9bf3daee8ef40e1b70b636bd
Philip Schatz <253202+philschatz@users.noreply.github.com>
2021-07-21 15:10:07 -0500

    all-git-* steps now require a CODE_VERSION

--- test/test-step-02.bash
+++ test/test-step-02.bash
@@ -8,6 +8,7 @@ BOOK_DIR=../data/test-book
 # Build git PDF and web
 SKIP_DOCKER_BUILD=1 \
 KCOV_DIR=_kcov02-a \
+CODE_VERSION=main \
 ../cli.sh $BOOK_DIR all-git-pdf 'philschatz/tiny-book/book-slug1' chemistry main
 
 SKIP_DOCKER_BUILD=1 \
--- test/test-step-03.bash
+++ test/test-step-03.bash
@@ -8,4 +8,5 @@ BOOK_DIR=../data/test-book
 SKIP_DOCKER_BUILD=1 \
 KCOV_DIR=_kcov03 \
 START_AT_STEP=git-disassemble \
+CODE_VERSION=main \
 ../cli.sh $BOOK_DIR all-git-web 'philschatz/tiny-book/book-slug1' chemistry main
