1beed0049e1a337c1931a15bbbddd3338386ce4d
philschatz <253202+philschatz@users.noreply.github.com>
2021-07-27 17:34:36 -0500

    fix PDF test to load the images

--- test/test-step-07.bash
+++ test/test-step-07.bash
@@ -6,11 +6,11 @@ set -e
 SOCI_DIR=../data/test-soci
 
 # The all-archive-web checksum step mangles the assembled.xhtml file so we have to start over
-[[ -f $SOCI_DIR/assembled/collection.assembled.xhtml ]] && rm $SOCI_DIR/assembled/collection.assembled.xhtml
+[[ ! $CI_TEST && -f $SOCI_DIR/archive-book/collection.assembled.xhtml ]] && rm $SOCI_DIR/archive-book/collection.assembled.xhtml
 
 SKIP_DOCKER_BUILD=1 \
 KCOV_DIR=_kcov07-a \
-START_AT_STEP=archive-link-extras \
+START_AT_STEP=archive-assemble \
 ../cli.sh $SOCI_DIR all-archive-pdf
 
 SKIP_DOCKER_BUILD=1 \
