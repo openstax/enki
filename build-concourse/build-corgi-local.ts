57fc06b6436e473d5223211e29635c13a8625fb8
philschatz <253202+philschatz@users.noreply.github.com>
2021-11-19 19:48:49 -0600

    get local webhosting file to dequeue a book

--- build-concourse/build-corgi-local.ts
+++ build-concourse/build-corgi-local.ts
@@ -1,5 +1,5 @@
 import { randId, RANDOM_DEV_CODEVERSION_PREFIX } from './util'
 import { loadSaveAndDump} from './build-corgi'
 
-process.env['CODE_VERSION'] = `${RANDOM_DEV_CODEVERSION_PREFIX}-${randId}`
+process.env['CODE_VERSION'] = process.env['CODE_VERSION'] ?? `${RANDOM_DEV_CODEVERSION_PREFIX}-${randId}`
 loadSaveAndDump('./env/corgi-local.json', './corgi-local.yml')
