a811832a8a498edfbaf1eef1a9c3f9739213ff41
philschatz <253202+philschatz@users.noreply.github.com>
2021-12-01 18:17:26 -0600

    fix git-link so that it can work on multiple books

--- dockerfiles/steps/git-link.bash
+++ dockerfiles/steps/git-link.bash
@@ -1,26 +1,10 @@
 parse_book_dir
 
-repo_root=$IO_FETCH_META
-col_sep='|'
-# https://stackoverflow.com/a/31838754
-xpath_sel="//*[@slug]" # All the book entries
-if [[ $ARG_TARGET_SLUG_NAME ]]; then
-    xpath_sel="//*[@slug=\"$ARG_TARGET_SLUG_NAME\"]"
-fi
-while read -r line; do # Loop over each <book> entry in the META-INF/books.xml manifest
-    IFS=$col_sep read -r slug href style <<< "$line"
-    path="$repo_root/META-INF/$href"
+shopt -s globstar nullglob
+for collection in "$IO_BAKED/"*.baked.xhtml; do
+    slug_name=$(basename "$collection" | awk -F'[.]' '{ print $1; }')
 
-    # ------------------------------------------
-    # Available Variables: slug href style path
-    # ------------------------------------------
-    # --------- Code starts here
+    try link-single "$IO_BAKED" "$IO_BAKE_META" "$slug_name" "$IO_LINKED/$slug_name.linked.xhtml"
 
-
-
-    try link-single "$IO_BAKED" "$IO_BAKE_META" "$slug" "$IO_LINKED/$slug.linked.xhtml"
-
-
-
-    # --------- Code ends here
-done < <(try xmlstarlet sel -t --match "$xpath_sel" --value-of '@slug' --value-of "'$col_sep'" --value-of '@href' --value-of "'$col_sep'" --value-of '@style' --nl < $repo_root/META-INF/books.xml)
+done
+shopt -u globstar nullglob
