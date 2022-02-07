c260ff825342c8ee52c8f6a024b7643641d94797
KarinaMendez2 <13000365@galileo.edu>
2022-02-07 16:42:08 -0600

    more progress

--- dockerfiles/build/build-stage-recipes.sh
+++ dockerfiles/build/build-stage-recipes.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-cd $PROJECT_ROOT/recipes/
+cd $PROJECT_ROOT/cookbook/lib/recipes
 gem install bundler --no-document
 gem install byebug --no-document
 bundle config set no-cache 'true'
 bundle config set silence_root_warning 'true'
 
-$PROJECT_ROOT/recipes/scripts/install_used_gem_versions
+# $PROJECT_ROOT/cookbook/lib/recipes/scripts/install_used_gem_versions
