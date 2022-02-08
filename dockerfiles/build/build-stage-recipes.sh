4c1df38655c5b757da431e3f0b52900a72772386
KarinaMendez2 <13000365@galileo.edu>
2022-02-08 10:11:05 -0600

    finally working!!!

--- dockerfiles/build/build-stage-recipes.sh
+++ dockerfiles/build/build-stage-recipes.sh
@@ -4,14 +4,7 @@ set -e
 cd $PROJECT_ROOT/cookbook
 gem install bundler --no-document
 gem install byebug --no-document
-# TODO: Find a better way to install these. They were in install_used_gem_versions
-# gem install nokogiri --no-document
-# gem install slop --no-document
-gem install activesupport -v '< 6.1'
-gem install nokogiri -v '1.11.1'
-# gem install nokogiri -v '1.11.1-x86_64-linux'
+bundle install 
 
 bundle config set no-cache 'true'
 bundle config set silence_root_warning 'true'
-
-# $PROJECT_ROOT/cookbook/lib/recipes/scripts/install_used_gem_versions
