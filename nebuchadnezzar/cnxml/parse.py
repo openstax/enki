19e599e2df777dca772cb27bbea7d133b6a49ae1
philschatz <253202+philschatz@users.noreply.github.com>
2022-02-09 10:51:29 -0600

    :shirt: lint

--- nebuchadnezzar/cnxml/parse.py
+++ nebuchadnezzar/cnxml/parse.py
@@ -59,26 +59,41 @@ def _squash_to_text(elm, remove_namespaces=False):
     value = ''.join(value)
     return value
 
+
 def lookup_license_text(license_url):
     switcher = {
-        'http://creativecommons.org/licenses/by/1.0': 'Creative Commons Attribution License',
-        'http://creativecommons.org/licenses/by-nd/1.0': 'Creative Commons Attribution-NoDerivs License',
-        'http://creativecommons.org/licenses/by-nd-nc/1.0': 'Creative Commons Attribution-NoDerivs-NonCommercial License',
-        'http://creativecommons.org/licenses/by-nc/1.0': 'Creative Commons Attribution-NonCommercial License',
-        'http://creativecommons.org/licenses/by-sa/1.0': 'Creative Commons Attribution-ShareAlike License',
-        'http://creativecommons.org/licenses/by/2.0/': 'Creative Commons Attribution License',
-        'http://creativecommons.org/licenses/by-nd/2.0': 'Creative Commons Attribution-NoDerivs License',
-        'http://creativecommons.org/licenses/by-nd-nc/2.0': 'Creative Commons Attribution-NoDerivs-NonCommercial License',
-        'http://creativecommons.org/licenses/by-nc/2.0': 'Creative Commons Attribution-NonCommercial License',
-        'http://creativecommons.org/licenses/by-sa/2.0': 'Creative Commons Attribution-ShareAlike License',
-        'http://creativecommons.org/licenses/by/3.0/': 'Creative Commons Attribution License',
-        'http://creativecommons.org/licenses/by/4.0/': 'Creative Commons Attribution License',
-        'http://creativecommons.org/licenses/by-nc-sa/4.0/': 'Creative Commons Attribution-NonCommercial-ShareAlike License',
+        'http://creativecommons.org/licenses/by/1.0':
+            'Creative Commons Attribution License',
+        'http://creativecommons.org/licenses/by-nd/1.0':
+            'Creative Commons Attribution-NoDerivs License',
+        'http://creativecommons.org/licenses/by-nd-nc/1.0':
+            'Creative Commons Attribution-NoDerivs-NonCommercial License',
+        'http://creativecommons.org/licenses/by-nc/1.0':
+            'Creative Commons Attribution-NonCommercial License',
+        'http://creativecommons.org/licenses/by-sa/1.0':
+            'Creative Commons Attribution-ShareAlike License',
+        'http://creativecommons.org/licenses/by/2.0/':
+            'Creative Commons Attribution License',
+        'http://creativecommons.org/licenses/by-nd/2.0':
+            'Creative Commons Attribution-NoDerivs License',
+        'http://creativecommons.org/licenses/by-nd-nc/2.0':
+            'Creative Commons Attribution-NoDerivs-NonCommercial License',
+        'http://creativecommons.org/licenses/by-nc/2.0':
+            'Creative Commons Attribution-NonCommercial License',
+        'http://creativecommons.org/licenses/by-sa/2.0':
+            'Creative Commons Attribution-ShareAlike License',
+        'http://creativecommons.org/licenses/by/3.0/':
+            'Creative Commons Attribution License',
+        'http://creativecommons.org/licenses/by/4.0/':
+            'Creative Commons Attribution License',
+        'http://creativecommons.org/licenses/by-nc-sa/4.0/':
+            'Creative Commons Attribution-NonCommercial-ShareAlike License',
     }
     license_text = switcher.get(license_url, None)
     if license_text is None:
         raise Exception('unkonwn license')
     return license_text
+
 
 def parse_metadata(elm_tree):
     """Given an element-like object (:mod:`lxml.etree`)
