b6fd23d48dcb6e128883c5e3aa4bad32c711058c
Samuel Klutse <klutse.samuel@gmail.com>
2022-03-08 20:52:27 -0500

    Removed the frontend modules from the output-producer-service

--- output-producer-service/frontend/localConfig.js
+++
@@ -1,4 +0,0 @@
-export default {
-  apiBaseURL: process.env.API_URL || 'https://corgi.openstax.org',
-  browserBaseURL: process.env.API_URL_BROWSER || 'https://corgi.openstax.org'
-}
--- output-producer-service/frontend/plugins/axios.js
+++
@@ -1,7 +0,0 @@
-export default function ({ $axios, redirect }) {
-  $axios.onRequest((config) => {
-    console.log('Making request to ' + config.url)
-    config.headers['Content-Type'] = 'application/json'
-    return config
-  })
-}
