46fed4de23344f6be5d85a6d62ce1099cb13de35
Samuel Klutse <klutse.samuel@gmail.com>
2022-03-18 00:12:44 -0400

    Removed unused bash scripts from output-producer-service module.

--- output-producer-service/bakery/src/scripts/disassemble_single.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-exec > >(tee "${COMMON_LOG_DIR}"/log >&2) 2>&1
-
-slug_name=$(cat "${BOOK_INPUT}/slug")
-disassemble "${RESOURCE_LINKED_INPUT}/$slug_name.linked.xhtml" "${BAKED_BOOK_META_INPUT}/$slug_name.baked-metadata.json" "$slug_name" "${DISASSEMBLED_OUTPUT}"
--- output-producer-service/bakery/src/scripts/pdfify_single.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-exec > >(tee "${COMMON_LOG_DIR}"/log >&2) 2>&1
-
-cp "${STYLE_INPUT}"/* "${REXLINKED_INPUT}"
-
-echo -n "https://${BUCKET_NAME}.s3.amazonaws.com/$(cat "${BOOK_INPUT}"/pdf_filename)" > "${ARTIFACTS_OUTPUT}/pdf_url"
-prince -v --output="${ARTIFACTS_OUTPUT}/$(cat "${BOOK_INPUT}"/pdf_filename)" "${REXLINKED_INPUT}/$(cat "${BOOK_INPUT}"/slug).rex-linked.xhtml"
