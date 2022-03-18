fd45d1799c15889e72d10399c301398f89d45397
Samuel Klutse <klutse.samuel@gmail.com>
2022-03-18 01:16:43 -0400

    Test Code coverage results Upload

--- test/test-step-10.bash
+++ test/test-step-10.bash
@@ -8,10 +8,11 @@ COVERAGE_DIR=../coverage
 [[ -d $COVERAGE_DIR ]] || mkdir $COVERAGE_DIR
 pip install "../output-producer-service/bakery/src/scripts/.[test]"
 flake8 "../output-producer-service/bakery/src/scripts" --max-line-length=100
-pytest --cov=bakery_scripts --cov-append --cov-report=html --cov-report=term ../output-producer-service/ -vvv
+pytest --cov=bakery_scripts --cov-append --cov-report=html:$COVERAGE_DIR --cov-report=term ../output-producer-service/ -vvv
 
 # Upload to codecov only if running inside CI
 if [[ $CI || $CODECOV_TOKEN ]]; then
    echo "Upload Code Coverage Results!"
-   bash <(curl -s https://codecov.io/bash)
+   cd ../
+   bash <(curl -s https://codecov.io/bash) -s ./coverage
 fi
