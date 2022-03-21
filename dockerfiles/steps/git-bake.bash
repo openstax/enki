9d304cbb0288598fc2eac7ae95e16dbe33dce593
philschatz <253202+philschatz@users.noreply.github.com>
2022-03-21 15:52:20 -0500

    rename RECIPES_ROOT to COOKBOOK_ROOT

--- dockerfiles/steps/git-bake.bash
+++ dockerfiles/steps/git-bake.bash
@@ -19,7 +19,7 @@ for collection in "$IO_ASSEMBLED/"*.assembled.xhtml; do
     if [[ -f "$style_file" ]]
         then
             export VERBOSE=$TRACE_ON
-            try $RECIPES_ROOT/bake -b "$style_name" -i "$IO_ASSEMBLED/$slug_name.assembled.xhtml" -o "$IO_BAKED/$slug_name.baked.xhtml"
+            try $COOKBOOK_ROOT/bake -b "$style_name" -i "$IO_ASSEMBLED/$slug_name.assembled.xhtml" -o "$IO_BAKED/$slug_name.baked.xhtml"
             try sed -i "s%<\\/head>%<link rel=\"stylesheet\" type=\"text/css\" href=\"the-style-pdf.css\" />&%" "$IO_BAKED/$slug_name.baked.xhtml"
     fi
 done
