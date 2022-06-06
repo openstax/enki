1dced070771941d404a567b999731dc722e3aecc
therealmarv <1050582+therealmarv@users.noreply.github.com>
2022-06-06 17:36:54 +0300

    more verification for local-preview

--- test/test-step-03.bash
+++ test/test-step-03.bash
@@ -22,7 +22,11 @@ KCOV_DIR=_kcov03 \
 ../enki --keep-data --data-dir $BOOK_DIR --command local-preview
 
 # Check local-preview directories / files existing
-[ -d "$BOOK_DIR/local-preview" ] || echo "Directory $BOOK_DIR/local-preview missing"
-[ -d "$BOOK_DIR/local-preview/contents" ] || echo "Directory $BOOK_DIR/local-preview/contents missing"
-[ -d "$BOOK_DIR/local-preview/contents" ] || echo "Directory $BOOK_DIR/local-preview/contents missing"
-[[ -L "$BOOK_DIR/local-preview/resources" && -d "$BOOK_DIR/local-preview/resources" ]] || echo "Symlink and/or directory $BOOK_DIR/local-preview/resources missing"
+[ -d "$BOOK_DIR/local-preview" ] || echo "local-preview: Directory $BOOK_DIR/local-preview missing"
+[ -d "$BOOK_DIR/local-preview/contents" ] || echo "local-preview: Directory $BOOK_DIR/local-preview/contents missing"
+[ -d "$BOOK_DIR/local-preview/contents" ] || echo "local-preview: Directory $BOOK_DIR/local-preview/contents missing"
+[[ -L "$BOOK_DIR/local-preview/resources" && -d "$BOOK_DIR/local-preview/resources" ]] || echo "local-preview: Symlink and/or directory $BOOK_DIR/local-preview/resources missing"
+[ -e "$BOOK_DIR/local-preview/resources/4e88fcaf0d07298343a7cb933926c4c0c6b5b017" ] || echo "local-preview: Duck wearing hat photo missing"
+CONTENT_SAMPLE_FILE="$BOOK_DIR/local-preview/contents/00000000-0000-0000-0000-000000000000@458dfb7:11111111-1111-1111-1111-111111111111.xhtml"
+[ -e "$BOOK_DIR/local-preview/contents/00000000-0000-0000-0000-000000000000@458dfb7:11111111-1111-1111-1111-111111111111.xhtml" ] || echo "local-preview: $CONTENT_SAMPLE_FILE content file missing"
+grep -q "\<img src=\"..\/resources\/4e88fcaf0d07298343a7cb933926c4c0c6b5b017\"" "$CONTENT_SAMPLE_FILE" || echo "local-preview: content not found in content file"
