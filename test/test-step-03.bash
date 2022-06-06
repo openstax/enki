a28b4967e40a70679af8bab6c60f692068319aa8
therealmarv <1050582+therealmarv@users.noreply.github.com>
2022-06-06 16:47:38 +0300

    check directories

--- test/test-step-03.bash
+++ test/test-step-03.bash
@@ -14,3 +14,15 @@ KCOV_DIR=_kcov03 \
 SKIP_DOCKER_BUILD=1 \
 KCOV_DIR=_kcov03 \
 ../enki --keep-data --data-dir $BOOK_DIR --command all-git-web --repo 'philschatz/tiny-book' --book-slug 'book-slug1' --style chemistry --ref @458dfb710e9af3d00d6f7e0be45fc819b955d931 --start-at git-disassemble
+
+
+# Check local-preview works
+SKIP_DOCKER_BUILD=1 \
+KCOV_DIR=_kcov03 \
+../enki --keep-data --data-dir $BOOK_DIR --command local-preview
+
+# Check local-preview directories / files existing
+[ -d "$BOOK_DIR/local-preview" ] || echo "Directory $BOOK_DIR/local-preview missing"
+[ -d "$BOOK_DIR/local-preview/contents" ] || echo "Directory $BOOK_DIR/local-preview/contents missing"
+[ -d "$BOOK_DIR/local-preview/contents" ] || echo "Directory $BOOK_DIR/local-preview/contents missing"
+[[ -L "$BOOK_DIR/local-preview/resources" && -d "$BOOK_DIR/local-preview/resources" ]] || echo "Symlink and/or directory $BOOK_DIR/local-preview/resources missing"
