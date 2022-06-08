ab63cc54102fcc224f1450ed5d963bd5c05abece
Philip Schatz <253202+philschatz@users.noreply.github.com>
2022-06-08 13:06:10 -0500

    :fire: cnx-recipes and replace with ce-styles (#128)
    * :fire: cnx-recipes and replace with ce-styles
    * rename CNX_RECIPES_STYLES_ROOT to BOOK_STYLES_ROOT

--- dockerfiles/steps/archive-bake.bash
+++ dockerfiles/steps/archive-bake.bash
@@ -2,7 +2,7 @@ parse_book_dir
 
 try $COOKBOOK_ROOT/bake -b "$ARG_RECIPE_NAME" -i "$IO_ARCHIVE_BOOK/collection.linked.xhtml" -o "$IO_ARCHIVE_BOOK/collection.baked.xhtml"
 
-style_file="$CNX_RECIPES_STYLES_ROOT/$ARG_RECIPE_NAME-pdf.css"
+style_file="$BOOK_STYLES_ROOT/$ARG_RECIPE_NAME-pdf.css"
 
 if [ -f "$style_file" ]; then
     cp "$style_file" "$IO_ARCHIVE_BOOK"
