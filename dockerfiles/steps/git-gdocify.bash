4cb6c967b9bd1e87251b8150e5dabdcc81d0af18
Tyler Nullmeier <tylerzeromaster@gmail.com>
2022-07-13 15:51:45 +0000

    Use book slug as directory during docx conversion
    This is meant to preserve page slugs that share the same name between
    book slugs (i.e. 1-summary in volume 1 vs 1-summary in volume-2).
    Prior to this change, page slugs with the same name were being
    overwritten in the process of building bundles.
    
    Another option would be to add the book slug to the beginning of each
    file name ({book_slug}-{page_slug}.docx). I decided to use directories
    because it makes it easier to inspect the output of the build when
    needed.
    
    The other option might make more sense when uploading the files to
    an external service. If that happens, we can flatten the directories
    and add the book slug to the name of each docx file as it is uploaded.

--- dockerfiles/steps/git-gdocify.bash
+++ dockerfiles/steps/git-gdocify.bash
@@ -2,7 +2,7 @@
 [[ -d "$IO_GDOCIFIED/content" ]] || mkdir "$IO_GDOCIFIED/content"
 try cp -R "$IO_RESOURCES/." "$IO_GDOCIFIED/resources"
 
-book_slugs_file="/tmp/book-slugs.json"
+book_slugs_file="$IO_GDOCIFIED/content/book-slugs.json"
 
 jo_args=''
 
@@ -28,7 +28,8 @@ while read -r line; do # Loop over each <book> entry in the META-INF/books.xml m
 done < <(try xmlstarlet sel -t --match "$xpath_sel" --value-of '@slug' --value-of "'$col_sep'" --value-of '@href' --value-of "'$col_sep'" --value-of '@style' --nl < $repo_root/META-INF/books.xml)
 
 # Save all the slug/uuid pairs into a JSON file
-try jo -a $jo_args > $book_slugs_file
+# NOTE: This file is also used in convert-docx
+try jo -a $jo_args > "$book_slugs_file"
 
 try gdocify "$IO_JSONIFIED" "$IO_GDOCIFIED/content" "$book_slugs_file"
 try cp "$IO_DISASSEMBLE_LINKED"/*@*-metadata.json "$IO_GDOCIFIED/content"
