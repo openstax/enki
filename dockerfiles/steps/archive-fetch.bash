4ed1db6f660fade695937fa03e675183c718c4c5
philschatz <253202+philschatz@users.noreply.github.com>
2022-08-30 14:02:01 -0500

    :bug: fix CI (almost) still need to fix linter errors

--- dockerfiles/steps/archive-fetch.bash
+++ dockerfiles/steps/archive-fetch.bash
@@ -1,6 +1,9 @@
 parse_book_dir
 
 temp_dir=$(mktemp -d)
+
+set +E # The entrypoint script sets this to be -E which causes neb to fail
 yes | try neb get -r -d "$temp_dir/does-not-exist-yet-dir" "$ARG_ARCHIVE_SERVER" "$ARG_COLLECTION_ID" "$ARG_COLLECTION_VERSION"
+set -E
 
 try mv $temp_dir/does-not-exist-yet-dir/* $IO_ARCHIVE_FETCHED
--- test/test-step-04.bash
+++ test/test-step-04.bash
@@ -5,10 +5,10 @@ set -e
 
 SOCI_DIR=../data/test-soci
 
-SKIP_DOCKER_BUILD=1 \
+SKIP_DOCKER_BUILD=${SKIP_DOCKER_BUILD:-1} \
 KCOV_DIR=_kcov04-a \
 ../enki --clear-data --data-dir $SOCI_DIR --command all-archive-web --repo col11762 --style sociology --ref latest
 
-SKIP_DOCKER_BUILD=1 \
+SKIP_DOCKER_BUILD=${SKIP_DOCKER_BUILD:-1} \
 KCOV_DIR=_kcov04-b \
 ../enki --keep-data --data-dir $SOCI_DIR --command archive-validate-cnxml
