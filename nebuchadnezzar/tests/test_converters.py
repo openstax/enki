9e4d19833e166838158ad47e8266aa5f9c3511a2
philschatz <253202+philschatz@users.noreply.github.com>
2022-10-03 11:29:28 -0500

    :fire: dead code it was used by archive tools but is no longer used

--- nebuchadnezzar/tests/test_converters.py
+++ nebuchadnezzar/tests/test_converters.py
@@ -7,15 +7,10 @@
 # ###
 import os
 import sys
-from io import BytesIO
-
-from lxml import etree
 
 from cnxtransforms.converters import (
-    DEFAULT_XMLPARSER,
     cnxml_abstract_to_html,
     cnxml_to_full_html,
-    html_to_full_cnxml,
 )
 
 
@@ -58,32 +53,6 @@ def test_cnxml_abstract_to_html():
         expected_abstract = expected_abstract.encode('utf-8')
 
     assert converted_abstract == expected_abstract
-
-
-class TestHtml2Cnxml(BaseTestCase):
-
-    def test_success(self):
-        # Case to test the transformation of cnxml to html.
-        html = self.get_file('m42033-1.3.html')
-
-        content = html_to_full_cnxml(html)
-
-        # Check for partial conversion.
-        # rhaptos.cnxmlutils has tests to ensure full conversion.
-        assert b'<document' in content
-
-    def test_with_passed_in_etree(self):
-        # Case to test the transformation of cnxml to html.
-        html = self.get_file('m42033-1.3.html')
-        if sys.version_info > (3,) and isinstance(html, str):
-            html = html.encode('utf-8')
-        html = etree.parse(BytesIO(html), DEFAULT_XMLPARSER)
-
-        content = html_to_full_cnxml(html)
-
-        # Check for partial conversion.
-        # rhaptos.cnxmlutils has tests to ensure full conversion.
-        assert b'<document' in content
 
 
 def get_file(filename):
