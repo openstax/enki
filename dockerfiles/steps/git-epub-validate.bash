741ff54912f3bfc975bec766ffd0f127c55addc6
Philip Schatz <253202+philschatz@users.noreply.github.com>
2022-12-20 11:31:29 -0600

    :skull: ignore the "aside" error for bulk testing

--- dockerfiles/steps/git-epub-validate.bash
+++ dockerfiles/steps/git-epub-validate.bash
@@ -28,7 +28,7 @@ for epub_file in "$IO_ARTIFACTS/"*.epub; do
         #     | grep -v 'ERROR(RSC-012)' \
         #     | grep -v 'ERROR(MED-002)' \
         # )
-        errors=$(cat $IO_ARTIFACTS/$epub_filename.validation.log | grep 'ERROR' | grep -v '.epub/the-style-epub.css'
+        errors=$(cat $IO_ARTIFACTS/$epub_filename.validation.log | grep 'ERROR' | grep -v '.epub/the-style-epub.css' | grep -v 'Error while parsing file: element "aside" not allowed here'
         )
 
         if [[ $errors ]]; then
