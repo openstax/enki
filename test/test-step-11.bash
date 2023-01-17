b8712f12efe07c50472703b1507969862c4b1017
Philip Schatz <253202+philschatz@users.noreply.github.com>
2023-01-17 15:46:15 -0600

    use different book so tests pass

--- test/test-step-11.bash
+++ test/test-step-11.bash
@@ -3,8 +3,8 @@ set -e
 [[ $TRACE_ON ]] && set -x
 [[ $0 != "-bash" ]] && cd "$(dirname "$0")"
 
-VIRO_DIR=../data/test-viro
+BUSI_DIR=../data/test-busi
 
 SKIP_DOCKER_BUILD=${SKIP_DOCKER_BUILD:-1} \
 KCOV_DIR=_kcov11 \
-../enki --clear-data --data-dir $VIRO_DIR --command all-git-epub --repo 'cnx-user-books/cnxbook-virology' --book-slug virology --style dummy --ref main
+../enki --clear-data --data-dir $BUSI_DIR --command all-git-epub --repo 'openstax/osbooks-business-law' --book-slug business-law-i-essentials --style default --ref main
