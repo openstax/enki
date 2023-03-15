6f439087d3858993d04c81bf3f5df3ee1c073272
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-03-15 22:06:17 +0000

    :fire: Remove tests that were based on old standards

--- nebuchadnezzar/nebu/tests/data/collection_for_bakedpdf_workflow/README.md
+++
@@ -1,7 +0,0 @@
-## Purpose
-
-This collection was created for the purpose of testing the Baked PDF workflow: cnxml -> html -> single-html.
-
-Please do add elements to this test data if they fit into the Baked PDF workflow.
-
-Note, this content is used to generate the content in `../assembled_collection_for_bakedpdf_workflow`. If you make changes here, please see the [README](../assembled_collection_for_bakedpdf_workflow) in that directory for instructions.
--- nebuchadnezzar/nebu/tests/test_parse.py
+++ nebuchadnezzar/nebu/tests/test_parse.py
@@ -5,46 +5,10 @@ from nebu.parse import NSMAP, parse_metadata
 
 
 @pytest.fixture
-def xml(datadir):
-    with (datadir / 'valid_collection.xml').open() as fb:
-        xml = etree.parse(fb)
-    return xml
-
-
-@pytest.fixture
 def git_xml(datadir):
     with (datadir / 'valid_git_collection.xml').open() as fb:
         xml = etree.parse(fb)
     return xml
-
-
-def test_parse(xml):
-    # Call the target
-    props = parse_metadata(xml)
-
-    expected_props = {
-        'abstract': (
-            'This introductory, algebra-based, two-semester college physics '
-            'book is grounded with real-world examples, illustrations, and '
-            'explanations to help students grasp key, fundamental physics '
-            'concepts. This online, fully editable and customizable title '
-            'includes learning objectives, concept questions, links to labs '
-            'and simulations, and ample practice opportunities to solve '
-            'traditional physics application problems.'
-        ),
-        'id': 'col11406',
-        'language': 'en',
-        'license_url': 'http://creativecommons.org/licenses/by/4.0/',
-        'license_text': 'Creative Commons Attribution License',
-        'revised': '2015/07/27 12:55:32.442 GMT-5',
-        'title': 'College Physics',
-        'version': '1.9',
-        'uuid': None,
-        'canonical_book_uuid': None,
-        'slug': None,
-    }
-    # Verify the metadata
-    assert props == expected_props
 
 
 def test_git_parse(git_xml):
@@ -66,63 +30,6 @@ def test_git_parse(git_xml):
     }
     # Verify the metadata
     assert props == expected_props
-
-
-# https://github.com/Connexions/cnx-press/issues/17
-def test_parse_without_abstract(xml):
-    # Remove the abstract
-    elm = xml.xpath('//md:abstract',
-                    namespaces=NSMAP)[0]
-    elm.getparent().remove(elm)
-
-    # Call the target
-    props = parse_metadata(xml)
-
-    # Verify the abstract is empty
-    assert props['abstract'] is None
-
-
-def test_parse_with_cnxml_abstract(xml):
-    abstract = '<list><item>A</item><item>C</item><item>E</item></list>'
-
-    # Find and modify the abstract to include wrapping text
-    elm = xml.xpath('//md:abstract', namespaces=NSMAP)[0]
-    elm.text = "FOO "
-    abstract_elms = etree.fromstring(abstract)
-    abstract_elms.tail = " BAR"
-    elm.append(abstract_elms)
-
-    elm.tail = " BAR"
-
-    # Call the target
-    props = parse_metadata(xml)
-
-    expected_abstract = 'FOO {} BAR'.format(abstract)
-    # parse the metadata into a dict and check for the abstract.
-    assert props['abstract'] == expected_abstract
-
-
-def test_parse_derived_from(xml):
-    uri = 'https://example.org/content/col12345/1.11'
-    title = 'Foo Bar'
-
-    derived_from = (
-        '<derived-from xmlns="{}" url="{}">'
-        '<title>{}</title>'
-        '</derived-from>'
-    ).format(NSMAP['md'], uri, title)
-
-    # Find and modify the metadata
-    elm = xml.xpath('//col:metadata', namespaces=NSMAP)[0]
-    # Create md:derive-from element and md:title sub-element
-    derived_from_elms = etree.fromstring(derived_from)
-    elm.append(derived_from_elms)
-
-    # Call the target
-    props = parse_metadata(xml)
-
-    # Verify we've discovered the correct title
-    assert props['title'] == 'College Physics'
 
 
 def test_parse_with_minimal_metadata():
