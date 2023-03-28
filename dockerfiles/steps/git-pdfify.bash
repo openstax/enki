443a12f1fbc805714dd26ca83ec06d415ca54a32
Kendra Swanson <kendraswan@gmail.com>
2023-03-28 12:04:01 -0400

    make pdfify generate for all slugs if none provided

--- dockerfiles/steps/git-pdfify.bash
+++ dockerfiles/steps/git-pdfify.bash
@@ -1,6 +1,14 @@
 parse_book_dir
 
-try prince -v --output="$IO_ARTIFACTS/$ARG_TARGET_PDF_FILENAME" "$IO_REX_LINKED/$ARG_TARGET_SLUG_NAME.rex-linked.xhtml"
+if [[ $ARG_TARGET_SLUG_NAME ]]; then
+  try prince -v --output="$IO_ARTIFACTS/$ARG_TARGET_PDF_FILENAME" "$IO_REX_LINKED/$ARG_TARGET_SLUG_NAME.rex-linked.xhtml"
+else
+  rex_linked=($(ls "$IO_REX_LINKED/"*".xhtml"))
+  for file in "${rex_linked[@]}"; do
+    slug=$(basename $file | awk -F'[.]' '{ print $1; }') # from git-jsonify
+    try prince -v --output="$IO_ARTIFACTS/$slug.pdf" "$file"
+  done
+fi
 
 # Verify the style file exists before building a PDF
 # LCOV_EXCL_START
