932782143962ca8749d76af044af9e6be87c3a99
Samuel Klutse <klutse.samuel@gmail.com>
2023-04-18 10:59:55 -0400

    Fixed SC2068 errors

--- dockerfiles/steps/step-epub.bash
+++ dockerfiles/steps/step-epub.bash
@@ -6,13 +6,13 @@ set -Eeuxo pipefail
 # Ensure $IO_EPUB is empty
 [[ -d $IO_EPUB ]] && rm -rf ${IO_EPUB:?}/*
 
-extra_vars=()
+extra_vars=""
 (( JS_DEBUG==1 )) && {
     echo "Javascript debug mode is on"
-    extra_vars+=("--inspect-brk=0.0.0.0:9229")
+    extra_vars+="--inspect-brk=0.0.0.0:9229"
 }
 
-node --unhandled-rejections=strict  ${extra_vars[@]} "$JS_UTILS_STUFF_ROOT/bin/bakery-helper" epub ./ "$IO_EPUB/"
+node --unhandled-rejections=strict  $extra_vars "$JS_UTILS_STUFF_ROOT/bin/bakery-helper" epub ./ "$IO_EPUB/"
 
 shopt -s globstar nullglob
 for book_dir in "$IO_EPUB/"*; do
