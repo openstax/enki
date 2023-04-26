7089a8ced2d324c2bd0faf27a5e74dc898b176e9
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-04-26 15:42:48 -0500

    Simulate exercise math injection in tests

--- test/test-step-02.bash
+++ test/test-step-02.bash
@@ -19,6 +19,31 @@ KCOV_DIR=_kcov02-c \
 ../enki --keep-data --data-dir $BOOK_DIR --command all-pdf --repo 'philschatz/tiny-book' --book-slug 'book-slug1' --ref main # with slug
 
 # ################################
+# HACK: Add math to the cnxml to 
+# simulate exercise injection
+# (this is technically invalid cnxml)
+# ################################
+
+while read -r module_file; do
+    # Why not sed --in-place=.orig ? Because sed is different on MacOS
+    awk '{
+        if ($0 ~ /<\/content>/) {
+            print "<!-- HACK: injected math -->"
+            print "<div id=\"math-element\" data-math=\"\\frac{2}{5}\"></div>"
+        }
+        print
+    }' "$module_file" > "$module_file.math"
+    mv "$module_file" "$module_file.orig"
+    mv "$module_file.math" "$module_file"
+done < <(find $BOOK_DIR -name "index.cnxml")
+
+SKIP_DOCKER_BUILD=1 \
+KCOV_DIR=_kcov02-d \
+../enki --keep-data --data-dir $BOOK_DIR --command all-pdf --start-at step-prebake --repo tiny-book --book-slug book-slug1 --ref main
+
+find $BOOK_DIR -name "index.cnxml.orig" -exec bash -cxe 'mv $0 $(dirname $0)/index.cnxml' {} \;
+
+# ################################
 # Clone a branch, 
 # build the PDF URL,
 # and verify it is URL-encoded
