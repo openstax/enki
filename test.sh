0363881729d57840728d0b9ecfc5671ea1ace8cc
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-08-05 12:32:12 -0500

    Add test-step-07 to test workflow

--- test.sh
+++ test.sh
@@ -13,6 +13,7 @@ TRACE_ON=$TRACE_ON ./test/test-step-02.bash
 TRACE_ON=$TRACE_ON ./test/test-step-03.bash
 TRACE_ON=$TRACE_ON ./test/test-step-05.bash
 TRACE_ON=$TRACE_ON ./test/test-step-06.bash
+TRACE_ON=$TRACE_ON ./test/test-step-07.bash
 TRACE_ON=$TRACE_ON ./test/test-step-11.bash
 TRACE_ON=$TRACE_ON ./test/test-step-08.bash
 TRACE_ON=$TRACE_ON ./test/test-step-09.bash
